c1436a2979935bf52705208c3625dfa51a8f5f1d
ilya <ilya.selfrealization@gmail.com>
2021-11-24 00:03:02 +0300

    remove extra, small refactoring and added get samples

--- ammo-generator/ammo-generator.py
+++ ammo-generator/ammo-generator.py
@@ -4,21 +4,10 @@
 import sys
 import json
 
-# Change it!
-generate_json = True
+generate_format = "flact"
 
-
-def make_ammo(method, url, headers, case, body):
-    """ makes phantom ammo """
-    # http request w/o entity body template
-    req_template = (
-        "%s %s HTTP/1.1\r\n"
-        "%s\r\n"
-        "\r\n"
-    )
-
-    # http request with entity body template
-    req_template_w_entity_body = (
+# http request with entity body template
+req_template_w_entity_body = (
         "%s %s HTTP/1.1\r\n"
         "%s\r\n"
         "Content-Length: %d\r\n"
@@ -26,17 +15,23 @@ def make_ammo(method, url, headers, case, body):
         "%s\r\n"
     )
 
-    if not body:
-        req = req_template % (method, url, headers)
-    else:
-        req = req_template_w_entity_body % (method, url, headers, len(body), body)
-
-    # phantom ammo template
-    ammo_template = (
+# phantom ammo template
+ammo_template = (
         "%d %s\n"
         "%s"
     )
 
+method = "POST"
+case = ""
+headers = "Host: test.com\r\n" + \
+          "User-Agent: tank\r\n" + \
+          "Accept: */*\r\n" + \
+          "Connection: Close\r\n" 
+
+
+def make_ammo(method, url, headers, case, body):
+    """ makes phantom ammo """
+    req = req_template_w_entity_body % (method, url, headers, len(body), body)
     return ammo_template % (len(req), case, req)
 
 
@@ -48,19 +43,11 @@ def generate_json():
         "description": "description",
         "status": True
     }
-    method = "POST"
     url = "/document/v1/stock/"
-    case = ""
-    headers = "Host: test.com\r\n" + \
-              "User-Agent: tank\r\n" + \
-              "Accept: */*\r\n" + \
-              "Connection: Close\r\n" + \
-              "Content-type: application/json"
-
+    h = headers + "Content-type: application/json"
     s1 = json.dumps(body)
-    ammo = make_ammo(method, url, headers, case, s1)
+    ammo = make_ammo(method, url, h, case, s1)
     sys.stdout.write(ammo)
-
     f2 = open("./ammo/ammo-json.txt", "w")
     f2.write(ammo)
 
@@ -68,25 +55,16 @@ def generate_json():
 def generate_binary():
     f = open("./flatc/new.uu", "r")
     body = f.read()
-    method = "POST"
     url = "/document/v2/stock/"
-    case = ""
-
-    headers = "Host: test.com\r\n" + \
-              "User-Agent: tank\r\n" + \
-              "Accept: */*\r\n" + \
-              "Connection: Close\r\n" + \
-              "Content-type: application/x-flatbuffers"
-
-    ammo = make_ammo(method, url, headers, case, body)
+    h = headers + "Content-type: application/x-flatbuffers"
+    ammo = make_ammo(method, url, h, case, body)
     sys.stdout.write(ammo)
-
     f2 = open("./ammo/ammo-fb-1.txt", "w")
     f2.write(ammo)
 
 
 if __name__ == "__main__":
-    if generate_json:
+    if generate_format == "json":
         generate_json()
     else:
         generate_binary()
