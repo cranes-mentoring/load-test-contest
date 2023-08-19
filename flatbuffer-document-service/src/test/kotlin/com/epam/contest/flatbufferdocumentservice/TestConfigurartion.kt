e5f445b2fb3e20ad78eed553eab7c862524d2d40
ilya <ilya.selfrealization@gmail.com>
2023-08-20 00:22:00 +0200

    [LT-2] remove extra services

--- flatbuffer-document-service/src/test/kotlin/com/epam/contest/flatbufferdocumentservice/TestConfigurartion.kt
+++
@@ -1,8 +0,0 @@
-package com.epam.contest.flatbufferdocumentservice
-
-import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.context.annotation.ComponentScan
-
-@TestConfiguration
-@ComponentScan(lazyInit = true)
-class TestConfiguration
--- yandex_tank/ammo-generator/ammo-generator.py
+++ yandex_tank/ammo-generator/ammo-generator.py
@@ -4,8 +4,6 @@
 import sys
 import json
 
-generate_format = "flact"
-
 # http request with entity body template
 req_template_w_entity_body = (
         "%s %s HTTP/1.1\r\n"
@@ -39,32 +37,16 @@ def generate_json():
     body = {
         "name": "content",
         "price": 1,
-        "amount": 2,
-        "description": "description",
-        "status": True
+        "description": "description"
     }
     url = "/document/v1/stock/"
     h = headers + "Content-type: application/json"
     s1 = json.dumps(body)
     ammo = make_ammo(method, url, h, case, s1)
     sys.stdout.write(ammo)
-    f2 = open("./ammo/ammo-json.txt", "w")
+    f2 = open("ammo/ammo-json.txt", "w")
     f2.write(ammo)
-
-
-def generate_binary():
-    f = open("./flatc/new.uu", "r")
-    body = f.read()
-    url = "/document/v2/stock/"
-    h = headers + "Content-type: application/x-flatbuffers"
-    ammo = make_ammo(method, url, h, case, body)
-    sys.stdout.write(ammo)
-    f2 = open("./ammo/ammo-fb-1.txt", "w")
-    f2.write(ammo)
-
 
 if __name__ == "__main__":
-    if generate_format == "json":
-        generate_json()
-    else:
-        generate_binary()
+    generate_json()
+
